0dbba6e6e0b4732c2a3ce9adff8acab0343b8ffc
srinu <srinivasubella161@gmail.com>
2025-12-05 15:14:55 +0530

    Products data is added

---
+++ src/projectData/Data.js
@@ -0,0 +1,494 @@
+// import firsts from "../images/products/boat110-1.png";
+import firsts from "../images/products/fourthSlider.png";
+import second from "../images/products/jbl660nc-2.png";
+import third from "../images/products/jbl660nc-3.png";
+import fourth from "../images/products/jbl660nc-4.png";
+import fifth from "../images/products/boat518-1.png";
+import seven from "../images/products/boat518-2.png";
+import eight from "../images/products/boat518-3.png";
+import nine from "../images/products/boat518-4.png";
+import ten from "../images/products/boat131-1.png";
+import elven from "../images/products/boat131-2.png";
+import twelve from "../images/products/boat131-3.png";
+import thirteen from "../images/products/secondSlider.png";
+import fourteen from "../images/products/boat110-1.png";
+import fifteen from "../images/products/boat110-2.png";
+import sisteen from "../images/products/boat110-3.png";
+import seventeen from "../images/products/boat110-4.png";
+import eigthteen from "../images/products/boat410-1.png";
+import nineteen from "../images/products/boat410-2.png";
+import twenty from "../images/products/boat410-3.png";
+import twentyone from "../images/products/boat410-4.png";
+import twentyTwo from "../images/products/jbl200bt-1.png";
+import twentyThree from "../images/products/jbl200bt-2.png";
+import twentyFour from "../images/products/jbl200bt-3.png";
+import twentyFive from "../images/products/jbl200bt-4.png";
+import twentySix from "../images/products/fourthSlider.png";
+import twentySeven from "../images/products/sonyXb910n-2.png";
+import twentyEight from "../images/products/sonyXb910n-3.png";
+import twentyNine from "../images/products/sonyXb910n-4.png";
+import thirteens from "../images/products/jbl760nc-1.png";
+import thirteenone from "../images/products/jbl760nc-2.png";
+import thirteenTwo from "../images/products/jbl760nc-3.png";
+import thirteenThree from "../images/products/jbl760nc-4.png";
+import thirteenfour from "../images/products/boat255r-1.png";
+import thirteenfive from "../images/products/boat255r-2.png";
+import thirteenSix from "../images/products/boat255r-3.png";
+import thirteenSeven from "../images/products/boat255r-4.png";
+import thirteenEight from "../images/products/jbl100-1.png";
+import thirteenNine from "../images/products/jbl100-2.png";
+import fourty from "../images/products/jbl100-3.png";
+import fourtyOne from "../images/products/jbl100-4.png";
+import fourtyTwo from "../images/products/sony1000xm4-1.png";
+import fourtyThree from "../images/products/sony1000xm4-2.png";
+import fourtyFour from "../images/products/sony1000xm4-3.png";
+import fourtyFive from "../images/products/sony1000xm4-4.png";
+import fourtySix from "../images/products/boat228-1.png";
+import fourtySeven from "../images/products/boat228-2.png";
+import fourtyEight from "../images/products/fourthSlider.png";
+import fourtyNine from "../images/products/boat228-4.png";
+import fifty from "../images/products/jbl-endu-1.png";
+import fiftyone from "../images/products/jbl-endu-2.png";
+import fiftytwo from "../images/products/jbl-endu-3.png";
+import fiftyThree from "../images/products/jbl-endu-4.png";
+import fiftyFour from "../images/products/boat203-1.png";
+import fiftyFive  from"../images/products/boat203-2.png";
+import fiftySix from "../images/products/boat203-3.png";
+import fiftySeven from "../images/products/boat203-4.png";
+import fiftyEight from "../images/products/sonyCh710n-1.png";
+import fiftyNine from "../images/products/sonyCh710n-2.png";
+import sixty from "../images/products/sonyCh710n-3.png";
+import sixtyOne from "../images/products/sonyCh710n-4.png";
+import sixtyTwo from "../images/products/jbl500bt-1.png";
+import sixtyThree from "../images/products/jbl500bt-2.png";
+import sixtyFour from"../images/products/jbl500bt-3.png";
+import sixtyFive from "../images/products/jbl500bt-4.png";
+import sixtySix from "../images/products/boat381-1.png";
+import sixtySeven from  "../images/products/boat381-2.png";
+import sixtyEight from "../images/products/boat381-2.png";
+import sixtyNine from "../images/products/boat381-4.png";
+import seventy from "../images/products/sony-ex14ap-1.png";
+import seventyOne from "../images/products/sony-ex14ap-2.png";
+import seventyTwo from "../images/products/sony-ex14ap-3.png";  
+import seventyThree from "../images/products/sony-ex14ap-4.png";
+import seventyFour from "../images/products/sonyXb400-1.png";
+import sevetyFive from "../images/products/sonyXb400-2.png";
+import seventySix from "../images/products/sonyXb400-3.png";
+import seventySeven from "../images/products/sonyXb400-4.png";       
+const productsData = [
+    {
+        id: 1,
+        tag: "hero-product",
+        tagline: "Keep the noise out, or in. You choose.",
+        heroImage: "/images/products/jbl660nc-1.png",
+        images: [
+            firsts,
+            second,
+            third,
+            fourth,
+        ],
+        brand: "JBL",
+        title: "JBL Live 660NC",
+        info: "Wireless Over-Ear NC Headphones",
+        category: "Headphones",
+        type: "Over Ear",
+        connectivity: "Wireless",
+        finalPrice: 9999,
+        originalPrice: 14999,
+        quantity: 1,
+        ratings: 1234,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 2,
+        tag: "featured-product",
+        images: [
+            fifth,
+            seven,
+            eight,
+            nine
+        ],
+        brand: "boAt",
+        title: "boAt Rockerz 518",
+        info: "On-Ear Wireless Headphones",
+        category: "Headphones",
+        type: "On Ear",
+        connectivity: "Wireless",
+        finalPrice: 1299,
+        originalPrice: 3990,
+        quantity: 1,
+        ratings: 1321,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 3,
+        tag: "hero-product",
+        tagline: "Featherweight for comfort all-day.",
+        heroImage: "/images/products/boat131-3.png",
+        images: [
+            ten,
+            elven,
+            twelve,
+           thirteen ,
+        ],
+        brand: "boAt",
+        title: "boAt Airdopes 131",
+        info: "Wireless In-Ear Earbuds",
+        category: "Earbuds",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 1099,
+        originalPrice: 2990,
+        quantity: 1,
+        ratings: 1244,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 4,
+        images: [
+            fourteen,
+            fifteen,
+            sisteen,
+            seventeen,
+        ],
+        brand: "boAt",
+        title: "boAt BassHeads 110",
+        info: "In-Ear Wired Earphones",
+        category: "Earphones",
+        type: "In Ear",
+        connectivity: "Wired",
+        finalPrice: 449,
+        originalPrice: 999,
+        quantity: 1,
+        ratings: 556,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 5,
+        images: [
+            eigthteen,
+            nineteen,
+            twenty,
+            twentyone,
+        ],
+        brand: "boAt",
+        title: "boAt Rockerz 410",
+        info: "Bluetooth & Wired On-Ear Headphones",
+        category: "Headphones",
+        type: "On Ear",
+        connectivity: "Bluetooth & Wired",
+        finalPrice: 1599,
+        originalPrice: 2990,
+        quantity: 1,
+        ratings: 1563,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 6,
+        images: [
+            twentyTwo,
+            twentyThree,
+            twentyFour,
+            twentyFive,
+        ],
+        brand: "JBL",
+        title: "JBL Live 200BT",
+        info: "In-Ear Wireless Neckbands",
+        category: "Neckbands",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 3699,
+        originalPrice: 5299,
+        quantity: 1,
+        ratings: 836,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 7,
+        tag: "hero-product",
+        tagline: "Give your favourite music a boost.",
+        heroImage: "/images/products/sonyxb910n-1.png",
+        images: [
+            twentySix,
+            twentySeven,
+            twentyEight,
+            twentyNine,
+        ],
+        brand: "Sony",
+        title: "Sony WH-XB910N",
+        info: "Wireless Over-Ear Headphones",
+        category: "Headphones",
+        type: "Over Ear",
+        connectivity: "Wireless",
+        finalPrice: 13489,
+        originalPrice: 19990,
+        quantity: 1,
+        ratings: 679,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 8,
+        tag: "featured-product",
+        images: [
+            thirteens,
+            thirteenone,
+            thirteenTwo,
+            thirteenThree,
+        ],
+        brand: "JBL",
+        title: "JBL Tune 760NC",
+        info: "Wireless Over-Ear NC Headphones",
+        category: "Headphones",
+        type: "Over Ear",
+        connectivity: "Wireless",
+        finalPrice: 5999,
+        originalPrice: 7999,
+        quantity: 1,
+        ratings: 755,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 9,
+        tag: "featured-product",
+        images: [
+            thirteenfour,
+            thirteenfive,
+            thirteenSix,
+            thirteenSeven,
+        ],
+        brand: "boAt",
+        title: "boAt Rockerz 255",
+        info: "In-Ear Wireless Neckbands",
+        category: "Neckbands",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 899,
+        originalPrice: 2990,
+        quantity: 1,
+        ratings: 1464,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+{
+        id: 10,
+        images: [
+            thirteenEight,
+            thirteenNine,
+            fourty,
+            fourtyOne,
+        ],
+        brand: "JBL",
+        title: "JBL Wave 100",
+        info: "In-Ear Truly Wireless Earbuds",
+        category: "Earbuds",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 2999,
+        originalPrice: 6999,
+        quantity: 1,
+        ratings: 801,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 11,
+        images: [
+            fourtyTwo,
+            fourtyThree,
+            fourtyFour,
+            fourtyFive
+        ],
+        brand: "Sony",
+        title: "Sony WF-1000XM4",
+        info: "Wireless In-Ear NC Headphones",
+        category: "Earbuds",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 19990,
+        originalPrice: 24990,
+        quantity: 1,
+        ratings: 382,
+        rateCount: 3,
+        path: "/product-details/",
+    },
+    {
+        id: 12,
+        images: [
+            fourtySix,
+            fourtySeven,
+            fourtyEight,
+            fourtyNine,
+        ],
+        brand: "boAt",
+        title: "boAt BassHeads 228",
+        info: "In-Ear Wired Earphones",
+        category: "Earphones",
+        type: "In Ear",
+        connectivity: "Wired",
+        finalPrice: 649,
+        originalPrice: 1190,
+        quantity: 1,
+        ratings: 1178,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 13,
+        tag: "featured-product",
+        images: [
+            fifty,
+            fiftyone,
+            fiftytwo,
+            fiftyThree,
+        ],
+        brand: "JBL",
+        title: "JBL Endurance Run Sports",
+        info: "In-Ear Wired Earphones",
+        category: "Earphones",
+        type: "In Ear",
+        connectivity: "Wired",
+        finalPrice: 999,
+        originalPrice: 1599,
+        quantity: 1,
+        ratings: 1144,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 14,
+        tag: "featured-product",
+        images: [
+            fiftyFour,
+            fiftyFive,
+            fiftySix,
+            fiftySeven,
+        ],
+        brand: "boAt",
+        title: "boAt Airdopes 203",
+        info: "In-Ear Truly Wireless Earbuds",
+        category: "Earbuds",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 1074,
+        originalPrice: 3999,
+        quantity: 1,
+        ratings: 1340,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 15,
+        images: [
+            fiftyEight,
+            fiftyNine,
+            sixty,
+            sixtyOne,
+        ],
+        brand: "Sony",
+        title: "Sony WH-CH710N",
+        info: "Wireless Over-Ear NC Headphones",
+        category: "Headphones",
+        type: "Over Ear",
+        connectivity: "Wireless",
+        finalPrice: 8520,
+        originalPrice: 14990,
+        quantity: 1,
+        ratings: 853,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 16,
+        images: [
+            sixtyTwo,
+            sixtyThree,
+            sixtyFour,
+            sixtyFive,
+        ],
+        brand: "JBL",
+        title: "JBL Tune 500BT",
+        info: "On-Ear Wireless Headphones",
+        category: "Headphones",
+        type: "On Ear",
+        connectivity: "Wireless",
+        finalPrice: 3282,
+        originalPrice: 3999,
+        quantity: 1,
+        ratings: 364,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 17,
+        images: [
+            sixtySix,
+           sixtySeven,
+            sixtyEight,
+            sixtyNine,
+        ],
+        brand: "boAt",
+        title: "boAt Airdopes 381",
+        info: "In-Ear Wireless Earbuds",
+        category: "Earbuds",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 1699,
+        originalPrice: 4990,
+        quantity: 1,
+        ratings: 1011,
+        rateCount: 5,
+        path: "/product-details/",
+    },
+    {
+        id: 18,
+        images: [
+            seventy,
+            seventyOne,
+            seventyTwo,
+            seventyThree
+        ],
+        brand: "Sony",
+        title: "Sony MDR-EX14AP",
+        info: "In-Ear Wired Earphones",
+        category: "Earphones",
+        type: "In Ear",
+        connectivity: "Wired",
+        finalPrice: 549,
+        originalPrice: 1290,
+        quantity: 1,
+        ratings: 530,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+    {
+        id: 19,
+        images: [
+            seventyFour,
+            sevetyFive,
+            seventySix,
+            seventySeven,
+        ],
+        brand: "Sony",
+        title: "Sony WI-XB400",
+        info: "Wireless Extra Bass In-Ear Neckbands",
+        category: "Neckbands",
+        type: "In Ear",
+        connectivity: "Wireless",
+        finalPrice: 2690,
+        originalPrice: 4990,
+        quantity: 1,
+        ratings: 474,
+        rateCount: 4,
+        path: "/product-details/",
+    },
+];
+
+export default productsData;
